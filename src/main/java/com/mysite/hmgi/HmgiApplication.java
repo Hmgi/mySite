098a163fa00a0cc244e8f5de865b22195f9de1f7
hanmingi <hk335078@naver.com>
2023-02-01 12:18:47 +0900

    Initial commit

---
+++ src/main/java/com/mysite/hmgi/HmgiApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.hmgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/hmgi/HmgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.hmgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
