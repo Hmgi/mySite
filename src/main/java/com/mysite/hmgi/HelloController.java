cc941ccbb71b67a44ebde2d46d7b0971618ba810
hanmingi <hk335078@naver.com>
2023-02-01 12:28:50 +0900

    [ADD] Lombok install and import lombok test

--- src/main/java/com/mysite/hmgi/HelloController.java
+++ src/main/java/com/mysite/hmgi/HelloController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.Data;
+
 @Controller
 public class HelloController {
     @GetMapping
     @ResponseBody
     public String hello(){
-        return "Hello";
+        return "test";
     }
 }
