0c81afac19b412e09c8299d7c251896557024113
hanmingi <hk335078@naver.com>
2023-02-03 12:25:28 +0900

    [UPD] update domain package
    -> make Answer & Question package

---
+++ src/main/java/com/mysite/hmgi/answer/Answer.java
@@ -0,0 +1,25 @@
+package com.mysite.hmgi.answer;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import com.mysite.hmgi.question.Question;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/mysite/hmgi/answer/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.mysite.hmgi.answer;
+
+import com.mysite.hmgi.answer.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
