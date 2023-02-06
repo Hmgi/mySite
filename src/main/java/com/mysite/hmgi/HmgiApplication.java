4c08a41a4698b9ae38bec490e9e936ea40751766
hanmingi <hk335078@naver.com>
2023-02-06 12:34:35 +0900

    [UPD] alignment

--- src/main/java/com/mysite/hmgi/HmgiApplication.java
+++ src/main/java/com/mysite/hmgi/HmgiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HmgiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HmgiApplication.class, args);
 	}
-
 }
